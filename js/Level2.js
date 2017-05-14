4469511148311095e062ec2adbbbf13c574c5003
Yida <yidayuan1028@gmail.com>
2017-05-14 13:29:59 -0400

    much better level2

--- js/Level2.js
+++ js/Level2.js
@@ -12,14 +12,14 @@ var blkHeight = 100;
 var characterWidth = 30;
 var characterHeight = 30;
 
-var dropZone1;
-var dropZone2;
-var dropZone3;
+var zoneFilled = [false, false, false, false];
+var dropZones = [];
 var dragPosition;
 
 var currInputs = [];		// an empty array for the inputs
 var result1;
 var result2;
+var result3;
 var finalRes;
 var enemies = [];
 
@@ -39,6 +39,25 @@ level2.prototype = {
 		background.width = 800;
 		background.height = 600;
 
+		for (i = 0; i < 4; i++) {
+			dropZones[i] = game.add.sprite(150, 200, 'white-block');
+			dropZones[i].width = blkWidth;
+			dropZones[i].height = blkHeight;
+		}
+
+		dropZones[0].x = 150;
+		dropZones[0].y = 200;
+
+		dropZones[1].x = 150;
+		dropZones[1].y = 400;
+
+		dropZones[2].x = 300;
+		dropZones[2].y = 300;
+
+		dropZones[3].x = 450;
+		dropZones[3].y = 300;
+
+		/*
 		dropZone1 = game.add.sprite(150, 200, 'white-block');
 		dropZone1.width = blkWidth;
 		dropZone1.height = blkHeight;
@@ -50,6 +69,15 @@ level2.prototype = {
 		dropZone3 = game.add.sprite(400, 300, 'white-block');
 		dropZone3.width = blkWidth;
 		dropZone3.height = blkHeight;
+		*/
+
+		notGate = game.add.sprite(50, 25, 'not-gate');
+		notGate.width = blkWidth;
+		notGate.height = blkHeight;
+
+		bufGate = game.add.sprite(175, 25, 'buffer-gate');
+		bufGate.width = blkWidth;
+		bufGate.height = blkHeight;
 
 		andGate = game.add.sprite(300, 25, 'and-gate');
 		andGate.width = blkWidth;
@@ -58,6 +86,12 @@ level2.prototype = {
 		orGate = game.add.sprite(425, 25, 'or-gate');
 		orGate.width = blkWidth;
 		orGate.height = blkHeight;
+
+		notGate.inputEnabled = true;
+		notGate.events.onInputDown.add(this.onGateClick, this);
+
+		bufGate.inputEnabled = true;
+		bufGate.events.onInputDown.add(this.onGateClick, this);
 
 		andGate.inputEnabled = true;
 		andGate.events.onInputDown.add(this.onGateClick, this);
@@ -126,20 +160,49 @@ level2.prototype = {
 	},
 
 	onDragStop: function(sprite, pointer) {
-		if (sprite.overlap(dropZone1)) {
-			sprite.x = dropZone1.x;
-			sprite.y = dropZone1.y;
-			this.showResult(sprite, 1);
-		} else if (sprite.overlap(dropZone2)) {
-			sprite.x = dropZone2.x;
-			sprite.y = dropZone2.y;
-			this.showResult(sprite, 2);
-		} else if (sprite.overlap(dropZone3)) {
-			// TODO: disabled until drapzone 1 and 2 are filled
-			sprite.x = dropZone3.x;
-			sprite.y = dropZone3.y;
-			this.showResult(sprite, 3);
-			this.judgment();
+		// Note: dropZone 0, 1, 2 accepts or/and, 3 accepts not/buffer
+		if (sprite.overlap(dropZones[0])) {
+			if (sprite.key == 'not-gate' || sprite.key == 'buffer-gate') {
+				sprite.kill();
+			} else {
+				sprite.x = dropZones[0].x;
+				sprite.y = dropZones[0].y;
+				zoneFilled[0] = true;
+				this.showResult(sprite, 1);
+			}
+		} else if (sprite.overlap(dropZones[1])) {
+			if (sprite.key == 'not-gate' || sprite.key == 'buffer-gate') {
+				sprite.kill();
+			} else {
+				sprite.x = dropZones[1].x;
+				sprite.y = dropZones[1].y;
+				zoneFilled[1] = true;
+				this.showResult(sprite, 2);
+			}
+		} else if (sprite.overlap(dropZones[2])) {
+			// disabled until dropzone 0, 1 are filled
+			if (zoneFilled[0] && zoneFilled[1]) {
+				if (sprite.key == 'not-gate' || sprite.key == 'buffer-gate') {
+					sprite.kill();
+				} else {
+					sprite.x = dropZones[2].x;
+					sprite.y = dropZones[2].y;
+					zoneFilled[2] = true;
+					this.showResult(sprite, 3);
+				}
+			} else {
+				sprite.kill();
+			}
+		} else if (sprite.overlap(dropZones[3])) {
+			// disabled until drapzone[2] is filled
+			if (zoneFilled[2] && (sprite.key == 'not-gate' || sprite.key == 'buffer-gate')) {
+				sprite.x = dropZones[3].x;
+				sprite.y = dropZones[3].y;
+				this.showResult(sprite, 4);
+				this.judgment();
+			} else {
+				sprite.kill();
+			}
 		} else {
 			sprite.kill();
 		}
@@ -173,79 +236,113 @@ level2.prototype = {
 		}
 	},
 
+	setSpriteParams: function(sprite, x, y, width, height) {
+		sprite.x = x;
+		sprite.y = y;
+		sprite.width = width;
+		sprite.height = height;
+	},
+
+	bufferGateOutput: function(sprite) {
+		// accepts a sprite and pass it
+		var bufRes;
+		if (sprite.key == "white-knight")
+			bufRes = game.add.sprite(0, 0, 'white-knight');
+		else if (sprite.key == "grey-knight")
+			bufRes = game.add.sprite(0, 0, 'grey-knight');
+		return bufRes;
+	},
+
+	notGateOutput: function(sprite) {
+		// accepts a sprite and revert it
+		var notRes;
+		if (sprite.key == "white-knight")
+			notRes = game.add.sprite(0, 0, 'grey-knight');
+		else if (sprite.key == "grey-knight")
+			notRes = game.add.sprite(0, 0, 'white-knight');
+		return notRes;
+	},
+
+	andGateOutput: function(sprites) {
+		// accepts an array of sprites and return a result, the params of the result is up-to the game
+		var andRes;
+		for (i = 0; i < sprites.length; i++) {
+			// and gates return a white knight when all the items in the array are white
+			if (sprites.key != "white-knight") {
+				// return a grey knight
+				andRes = game.add.sprite(0, 0, 'grey-knight');
+				return andRes;
+			}
+		}
+		andRes = game.add.sprite(0, 0, 'white-knight');
+		return andRes;
+	},
+
+	orGateOutput: function(sprites) {
+		// accepts an array of sprites and return a result, the params of the result is up-to the game
+		var orRes;
+		for (i = 0; i < sprites.length; i++) {
+			// or gates return a grey knight when all the items in the array are grey
+			if (sprites.key != "grey-knight") {
+				// return a white knight
+				orRes = game.add.sprite(0, 0, 'white-knight');
+				return orRes;
+			}
+		}
+		orRes = game.add.sprite(0, 0, 'grey-knight');
+		return orRes;
+	},
+
 	showResult: function(sprite, num) {
+		var inputArray = [];
 		if (sprite.key == "and-gate") {
 	        // only produce white when both are white
 	        if (num == 1) {
 	            // curr1 and curr2
-	            if (currInputs[0].key == "white-knight" && currInputs[1].key == "white-knight") {
-	            	result1 = game.add.sprite(225, 200, 'white-knight');
-	            	result1.width = 100;
-	            	result1.height = 100;
-	            } else {
-	            	result1 = game.add.sprite(225, 200, 'grey-knight');
-	            	result1.width = 100;
-	            	result1.height = 100;
-	            }
+	            inputArray[0] = currInputs[0];
+	            inputArray[1] = currInputs[1];
+	            result1 = this.andGateOutput(inputArray);
+	            this.setSpriteParams(result1, 225, 200, 100, 100);
 	        } else if (num == 2) {
-	            // curr3 and curr4 
-	            if (currInputs[2].key == "white-knight" && currInputs[3].key == "white-knight") {
-	            	result2 = game.add.sprite(225, 400, 'white-knight');
-	            	result2.width = 100;
-	            	result2.height = 100;
-	            } else {
-	            	result2 = game.add.sprite(225, 400, 'grey-knight');
-	            	result2.width = 100;
-	            	result2.height = 100;
-	            }
+	            // curr3 and curr4
+	            inputArray[0] = currInputs[2];
+	            inputArray[1] = currInputs[3];
+	            result2 = this.andGateOutput(inputArray);
+	            this.setSpriteParams(result2, 225, 400, 100, 100);
 	        } else if (num == 3) {
 	            // result1 and result2
-	            if (result1.key == "white-knight" && result2.key == "white-knight") {
-	            	finalRes = game.add.sprite(500, 300, 'white-knight');
-	            	finalRes.width = 100;
-	            	finalRes.height = 100;
-	            } else {
-	            	finalRes = game.add.sprite(500, 300, 'grey-knight');
-	            	finalRes.width = 100;
-	            	finalRes.height = 100;
-	            }
+	            inputArray[0] = result1;
+	            inputArray[1] = result2;
+	            result3 = this.andGateOutput(inputArray);
+	            this.setSpriteParams(result3, 400, 300, 100, 100);
 	        }
 	    } else if (sprite.key == "or-gate") {
-	        // only produce grey when both are grey
+	        // only produce white when both are white
 	        if (num == 1) {
 	            // curr1 and curr2
-	            if (currInputs[0].key == "grey-knight" && currInputs[1].key == "grey-knight") {
-	            	result1 = game.add.sprite(225, 200, 'grey-knight');
-	            	result1.width = 100;
-	            	result1.height = 100;
-	            } else {
-	            	result1 = game.add.sprite(225, 200, 'white-knight');
-	            	result1.width = 100;
-	            	result1.height = 100;
-	            }
+	            inputArray[0] = currInputs[0];
+	            inputArray[1] = currInputs[1];
+	            result1 = this.orGateOutput(inputArray);
+	            this.setSpriteParams(result1, 225, 200, 100, 100);
 	        } else if (num == 2) {
-	            // curr3 and curr4 
-	            if (currInputs[2].key == "grey-knight" && currInputs[3].key == "grey-knight") {
-	            	result2 = game.add.sprite(225, 400, 'grey-knight');
-	            	result2.width = 100;
-	            	result2.height = 100;
-	            } else {
-	            	result2 = game.add.sprite(225, 400, 'white-knight');
-	            	result2.width = 100;
-	            	result2.height = 100;
-	            }
+	            // curr3 and curr4
+	            inputArray[0] = currInputs[2];
+	            inputArray[1] = currInputs[3];
+	            result2 = this.orGateOutput(inputArray);
+	            this.setSpriteParams(result2, 225, 400, 100, 100);
 	        } else if (num == 3) {
 	            // result1 and result2
-	            if (result1.key == "grey-knight" && result2.key == "balck-knight") {
-	            	finalRes = game.add.sprite(500, 300, 'grey-knight');
-	            	finalRes.width = 100;
-	            	finalRes.height = 100;
-	            } else {
-	            	finalRes = game.add.sprite(500, 300, 'white-knight');
-	            	finalRes.width = 100;
-	            	finalRes.height = 100;
-	            }
+	            inputArray[0] = result1;
+	            inputArray[1] = result2;
+	            result3 = this.orGateOutput(inputArray);
+	            this.setSpriteParams(result3, 400, 300, 100, 100);
 	        }
+	    } else if (sprite.key == "buffer-gate") {
+	    	finalRes = this.bufferGateOutput(result3);
+	        this.setSpriteParams(finalRes, 600, 300, 100, 100);
+	    } else if (sprite.key == "not-gate") {
+	    	finalRes = this.notGateOutput(result3);
+	        this.setSpriteParams(finalRes, 600, 300, 100, 100);
 	    }
 	},
 
